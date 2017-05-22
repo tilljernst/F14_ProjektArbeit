3443b2fce7bd4e541d5c1f878554ba18c9bce458
Till J. Ernst <tilljernst@gmail.com>
2017-05-22 10:34:12 +0200

    First commit, base project with researchkit

---
+++ F14_ProjektArbeit_Surveys/F14_ProjektArbeit_Surveys/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  F14_ProjektArbeit_Surveys
+//
+//  Created by Till J. Ernst on 22.05.17.
+//  Copyright © 2017 Till J. Ernst. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
